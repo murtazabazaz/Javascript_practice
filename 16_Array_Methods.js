6159494310b233fcdcbe837028675c9b28599601
murtaza <93817703+murtazabazaz@users.noreply.github.com>
2023-12-14 12:26:19 +0530

    Add files via upload

--- 16_Array_Methods.js
+++ 16_Array_Methods.js
@@ -1,7 +1,54 @@
 const prompt = require('prompt-sync')();
 
-let name = prompt("enter the name of the student -: ");
+// Array Methods...
 
-console.log(name);
+// toString Method..
+let a = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(a, typeof a);
 
+let b = a.toString(); //coverts array 'a' to string.
+console.log(b);
 
+for (let i = 0; i < a.length; i++) {
+    console.log(a[i]);
+
+}
+
+// Join Method...
+
+let c = a.join('_');
+console.log(c, typeof c);
+
+// pop Method..
+
+let d = a.pop();
+console.log(d, '\n' + a); // this method changes the array..
+
+// push Method..
+
+let e = a.push(89);
+console.log(a);
+
+// shift Method..
+
+let f = a.shift(); // it 
+console.log(a, '\n' + f); // removes the first element and returns the value.
+
+// unshift Method...
+
+let g = a.unshift(1);
+console.log(a + '\nNew array length = ' + g);
+
+// delete Method..
+
+delete a[1];
+console.log(a);
+
+// concat method..
+
+let a1 = [1, 2, 3];
+let b1 = [4, 5, 6];
+let c1 = [7, 8, 9];
+
+let newar = a1.concat(b1, c1);
+console.log(newar, typeof newar);
