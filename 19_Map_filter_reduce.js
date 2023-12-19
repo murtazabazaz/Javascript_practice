54d4866f812b45b7f4197d785b731587d39fbd9e
murtaza bazaz <93817703+murtazabazaz@users.noreply.github.com>
2023-12-19 18:51:20 +0530

    Add files via upload

--- 19_Map_filter_reduce.js
+++ 19_Map_filter_reduce.js
@@ -1,5 +1,5 @@
 const prompt = require('prompt-sync')();
-
+/*
 // Map, Filter & Reduce (high order array methods.)..
 function createarr(){
 
@@ -11,12 +11,12 @@ for(let i=0;i<size;i++){
    a[i] = prompt('Enter elements of array -: ');
 }
 };
-/*
+
 for(b in a){
     console.log('value at index = ', b +' is ' + a[b]);
 }
 
-*/
+
 
 // Map method(creates a new array instead of updting orginal array)...
 
@@ -28,3 +28,46 @@ let newarr = arr.map((value, index )=>{
 })
 
 console.log('the updated array after map method -: ', newarr ,'\nOld array before using map method -: ', arr);
+
+// Filter Method(creates a new array instead of updting orginal array)...
+
+let a1 = [];
+const s = prompt('enter size of array-: ');
+
+for( let i=0;i<s;i++){
+    a1[i]=prompt('enter the elements -: ')
+}
+
+var b = prompt('enter the number you want the values to be less than -: ');
+
+let n  = a1.filter((v)=>{
+    return v<b;
+})
+console.log('There are total '+ n.length + ' elements less '  + b + ' = ' +n)
+*/
+// filter example...
+//Code to search name in an array using filter method
+
+let a2 = [];
+const si = prompt('enter size of array-: ');
+
+for( let i=0;i<si;i++){
+ a2[i]=prompt('enter the name -: ')
+}
+
+var bw = prompt('enter the name you want to search-: ');
+
+let filteredNames = a2.filter((name)=> name == bw);
+
+
+    if(filteredNames.length>0){
+        return console.log('The Name '+ bw + ' matches ' + filteredNames[0] + ' in the array.')
+    }
+    
+       else{
+
+           console.log('name not found!!!')
+       }
+       
+       
+   
