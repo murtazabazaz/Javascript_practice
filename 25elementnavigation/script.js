22a465b114234a1892f718918613810bd5eeb0f6
murtaza bazaz <93817703+murtazabazaz@users.noreply.github.com>
2024-01-22 16:37:13 +0530

    Add files via upload

---
+++ 25elementnavigation/script.js
@@ -0,0 +1,6 @@
+let b = document.body;
+console.log("First child of b is: ", b.firstChild);
+console.log("First element child of b is: ", b.firstElementChild);
+let cc = document.getElementsByClassName('cl');
+console.log(cc);
+//cc.onclick(location.href = "https://google.com");
